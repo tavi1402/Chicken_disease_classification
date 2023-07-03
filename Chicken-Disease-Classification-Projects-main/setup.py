a9e96f0c2e1a5ec54ff1d5962fe84321176a2e3f
Tavishi <100408675+tavi1402@users.noreply.github.com>
2023-07-03 23:12:41 +0530

    Update setup.py

--- Chicken-Disease-Classification-Projects-main/setup.py
+++ Chicken-Disease-Classification-Projects-main/setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "tavishi14"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "tavishi.1402@gmail.com"
 
 
 setuptools.setup(
